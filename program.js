ca1461c42c410005e3b62aefbb4c16979d87b2e7
Kacper Stroisz <kacperstroisz@wp.pl>
2018-03-15 18:29:19 +0100

    Add file program.js

---
+++ program.js
@@ -0,0 +1,22 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+
+    var input = process.stdin.read();
+    if (input !== null) {
+        var instruction = input.toString().trim();
+        switch (instruction) {
+            case 'version':
+                process.stdout.write('Node version is: ');
+                break;
+            case 'language':
+                process.stdout.write('System language is ');
+                break;
+            case 'exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+                break;
+            default:
+                process.stderr.write('Wrong instruction!\n');
+        }
+    }
+});
